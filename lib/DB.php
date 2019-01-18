2cf3351118bbbcf879ef8be2189e45081026b59f
Willem Liu <willem_liu@hotmail.com>
2019-01-18 16:38:04 +0100

    Added DB

---
+++ lib/DB.php
@@ -0,0 +1,60 @@
+<?php
+  /**
+   * DB class
+   */
+  final class DB
+  {
+    private $connection = 0;
+    private $host = getenv('DB_HOST') ?: "host";
+    private $dbname = getenv('DB_NAME') ?: "dbname";
+    private $user = getenv('DB_USER') ?: "name";
+    private $pass = getenv('DB_PASS') ?: "pass";
+    private $dbh;
+    private $queryCount = 0;
+
+    /**
+     * Return the logged query count. This isn't accurate.
+     */
+    public function getQueryCount() {
+      return $this->queryCount;
+    }
+
+    /**
+     * Increment query count by 1.
+     */
+    public function incrementQueryCount() {
+      $this->queryCount++;
+    }
+
+    public function connect() {
+      try {
+        if($this->dbh == null) {
+          # MS SQL Server and Sybase with PDO_DBLIB
+          //$this->DBH = new PDO("mssql:host=$host;dbname=$dbname, $user, $pass");
+          //$this->DBH = new PDO("sybase:host=$host;dbname=$dbname, $user, $pass");
+
+          # MySQL with PDO_MYSQL
+          $this->dbh = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+
+          # SQLite Database
+          //$this->DBH = new PDO("sqlite:my/database/path/database.db");
+        }
+        return $this->dbh;
+      }
+      catch(PDOException $e) {
+        throw $e;
+      }
+    }
+
+    public function disconnect() {
+      $this->dbh = null;
+    }
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+  }
+?>
